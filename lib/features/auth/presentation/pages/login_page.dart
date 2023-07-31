76506eedfa624b7be57c8d6394ffb30c69e9bf31
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-31 07:43:57 +0500

    register page is done

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -6,6 +6,7 @@ import 'package:shopp/features/auth/presentation/providers/auth_provider.dart';
 import 'package:shopp/features/shopp/presentation/widgets/yellow_button.dart';
 
 import '../../../../config/router/routes.dart';
+import '../widgets/stattus_message_builder.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -22,90 +23,56 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF3E7E7),
-      body: Consumer<AuthProvider>(
-        builder: (context, authProvider, child) {
-          print('consumer');
-          return Stack(
-            fit: StackFit.expand,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/wandarLogo.png', height: MediaQuery.of(context).size.height * 0.3),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Image.asset('assets/images/wandarLogo.png', height: MediaQuery.of(context).size.height * 0.3),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        textFieldName('Email or Username'),
-                        TextField(
-                          style: const TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 16),
-                          cursorColor: const Color(0xFF6E75A8),
-                          controller: emailController,
-                          decoration: textfieldDecoration(Icons.email, 'Email@mail.ru'),
-                        ),
-                        const SizedBox(height: 16.0),
-                        textFieldName('Password'),
-                        TextField(
-                          cursorColor: const Color(0xFF6E75A8),
-                          controller: passwordController,
-                          decoration: textfieldDecoration(Icons.lock, 'Password'),
-                          obscureText: true,
-                        ),
-                      ],
+                    textFieldName('Email or Username'),
+                    TextField(
+                      style: const TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 16),
+                      cursorColor: const Color(0xFF6E75A8),
+                      controller: emailController,
+                      decoration: textfieldDecoration(Icons.email, 'Email@mail.ru'),
                     ),
                     const SizedBox(height: 16.0),
-                    YellowButton(
-                      title: 'Login',
-                      onTap: loginFunc,
+                    textFieldName('Password'),
+                    TextField(
+                      cursorColor: const Color(0xFF6E75A8),
+                      controller: passwordController,
+                      decoration: textfieldDecoration(Icons.lock, 'Password'),
+                      obscureText: true,
                     ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.08),
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                      const Text('Don\'t have an account? ', style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w400, fontSize: 14)),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, RouteGenerator.register);
-                        },
-                        child: const Text('Sign up', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 14)),
-                      ),
-                    ]),
                   ],
                 ),
-              ),
-              if (authProvider.status == Status.authenticating)
-                Container(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  color: Colors.black.withOpacity(0.5),
-                  child: const Center(child: CircularProgressIndicator()),
+                const SizedBox(height: 16.0),
+                YellowButton(
+                  title: 'Login',
+                  onTap: loginFunc,
                 ),
-              if (authProvider.status == Status.error)
-                Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  color: Colors.black.withOpacity(0.5),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text('Error', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 24)),
-                        const SizedBox(height: 16.0),
-                        Text(authProvider.error ?? 'Unknown error', style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w400, fontSize: 16)),
-                        const SizedBox(height: 16.0),
-                        TextButton(
-                          onPressed: () {
-                            authProvider.reset();
-                          },
-                          child: const Text('Try again', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 14)),
-                        ),
-                      ],
-                    ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  const Text('Don\'t have an account? ', style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w400, fontSize: 14)),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, RouteGenerator.register);
+                    },
+                    child: const Text('Sign up', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 14)),
                   ),
-                ),
-            ],
-          );
-        },
+                ]),
+              ],
+            ),
+          ),
+          const StatusMessageBuilder(),
+        ],
       ),
     );
   }
@@ -129,6 +96,7 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   void loginFunc() async {
+    // TODO: Check user has entered data correctly
     final AuthProvider authProvider = Provider.of<AuthProvider>(context, listen: false);
     bool isAdmin = await authProvider.login(emailController.text, passwordController.text);
     if (isAdmin && authProvider.status == Status.authenticated) {
--- lib/features/auth/presentation/pages/registration_page.dart
+++ lib/features/auth/presentation/pages/registration_page.dart
@@ -1,63 +1,77 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shopp/config/router/routes.dart';
 import 'package:shopp/features/shopp/presentation/widgets/yellow_button.dart';
 
-class RegistrationPage extends StatelessWidget {
+import '../providers/auth_provider.dart';
+import '../widgets/stattus_message_builder.dart';
+
+class RegistrationPage extends StatefulWidget {
+  RegistrationPage({super.key});
+
+  @override
+  State<RegistrationPage> createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController passwordController2 = TextEditingController();
-
-  RegistrationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF3E7E7),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/wandarLogo.png', height: MediaQuery.of(context).size.height * 0.3),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                textFieldName('Email or Username'),
-                TextField(
-                  style: const TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 16),
-                  cursorColor: const Color(0xFF6E75A8),
-                  controller: emailController,
-                  decoration: textfieldDecoration(Icons.email, 'Email@mail.ru'),
+                Image.asset('assets/images/wandarLogo.png', height: MediaQuery.of(context).size.height * 0.3),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    textFieldName('Email or Username'),
+                    TextField(
+                      style: const TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 16),
+                      cursorColor: const Color(0xFF6E75A8),
+                      controller: emailController,
+                      decoration: textfieldDecoration(Icons.email, 'Email@mail.ru'),
+                    ),
+                    const SizedBox(height: 16.0),
+                    textFieldName('Password'),
+                    TextField(
+                      cursorColor: const Color(0xFF6E75A8),
+                      controller: passwordController,
+                      decoration: textfieldDecoration(Icons.lock, 'Password'),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 16.0),
+                    textFieldName('reenter Password'),
+                    TextField(
+                      cursorColor: const Color(0xFF6E75A8),
+                      controller: passwordController2,
+                      decoration: textfieldDecoration(Icons.lock, 'reenter Password'),
+                      obscureText: true,
+                    ),
+                  ],
                 ),
                 const SizedBox(height: 16.0),
-                textFieldName('Password'),
-                TextField(
-                  cursorColor: const Color(0xFF6E75A8),
-                  controller: passwordController,
-                  decoration: textfieldDecoration(Icons.lock, 'Password'),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 16.0),
-                textFieldName('reenter Password'),
-                TextField(
-                  cursorColor: const Color(0xFF6E75A8),
-                  controller: passwordController2,
-                  decoration: textfieldDecoration(Icons.lock, 'reenter Password'),
-                  obscureText: true,
-                ),
+                YellowButton(
+                    title: 'Sign up',
+                    onTap: registerFunc),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [const Text('already have a account ', style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w400, fontSize: 14)), TextButton(onPressed: () {}, child: const Text('Sign in', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 14)))]),
               ],
             ),
-            const SizedBox(height: 16.0),
-            YellowButton(
-                title: 'Sign up',
-                onTap: () {
-                  Navigator.pushNamed(context, RouteGenerator.home);
-                }),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.08),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [const Text('already have a account ', style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w400, fontSize: 14)), TextButton(onPressed: () {}, child: const Text('Sign in', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 14)))]),
-          ],
-        ),
+          ),
+          const StatusMessageBuilder(),
+        ],
       ),
     );
   }
@@ -71,14 +85,24 @@ class RegistrationPage extends StatelessWidget {
 
   InputDecoration textfieldDecoration(icon, String hint) {
     return InputDecoration(
-        prefixIcon: Icon(
-          icon,
-          color: Colors.grey,
-        ),
-        enabledBorder: OutlineInputBorder(borderSide: const BorderSide(width: 1.5, color: Color(0xFFFF9A62)), borderRadius: BorderRadius.circular(20)),
-        focusedBorder: OutlineInputBorder(borderSide: const BorderSide(width: 3, color: Color(0xFFFF9A62)), borderRadius: BorderRadius.circular(20)),
-        errorBorder: OutlineInputBorder(borderSide: const BorderSide(width: 3, color: Colors.red), borderRadius: BorderRadius.circular(20)),
-        hintText: hint,
-        hintStyle: const TextStyle(color: Colors.grey, fontWeight: FontWeight.w600));
+      prefixIcon: Icon(icon, color: Colors.grey),
+      enabledBorder: OutlineInputBorder(borderSide: const BorderSide(width: 1.5, color: Color(0xFFFF9A62)), borderRadius: BorderRadius.circular(20)),
+      focusedBorder: OutlineInputBorder(borderSide: const BorderSide(width: 3, color: Color(0xFFFF9A62)), borderRadius: BorderRadius.circular(20)),
+      errorBorder: OutlineInputBorder(borderSide: const BorderSide(width: 3, color: Colors.red), borderRadius: BorderRadius.circular(20)),
+      hintText: hint,
+      hintStyle: const TextStyle(color: Colors.grey, fontWeight: FontWeight.w600),
+    );
+  }
+
+  void registerFunc() async {
+    // TODO: Check before registering that user passwords are correct and not empty
+    final AuthProvider authProvider = Provider.of<AuthProvider>(context, listen: false);
+    bool isAdmin = await authProvider.register(emailController.text, passwordController.text);
+    if (isAdmin && authProvider.status == Status.authenticated) {
+      // TODO: change to admin page
+      Navigator.pushNamed(context, RouteGenerator.profilePage);
+    } else if (authProvider.status == Status.authenticated) {
+      Navigator.pushNamed(context, RouteGenerator.home);
+    }
   }
 }
